1afa64f9b1fc20f8f0b63a1bfd4543d2f3dfe0fa
Jeff Miclos <jeffmicklos@gmail.com>
2010-04-04 22:00:03 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,35 @@
+Hey cool kids, this is a small MooTools class for paging through posts similar to FFFFOUND.
+
+Options:
+
+ar: 	   null		// An array of DOM elements, such as $$('.post') or $(document.body).getElements('.post')
+populate:  null		// Your function that injects new posts into the DOM
+keyEvent: 'keydown'	// The key action to listen to, 'keydown' or 'keyup' 
+loop:	   false	// Whether to loop through the current DOM elements without polling for new ones.
+
+Instantiation:
+There are two different ways to run the class. One being to loop through current posts on the page and the second being to pass in a function that polls for new posts, like Twitter's "view more" bar.
+
+<script type="text/javascript">
+
+//Case 1: Looping
+new JustKidding({
+		ar: $(document.body).getElements('.post'),
+		loop: true
+		});
+
+//Case 2:
+
+function viewmore(){
+	var el = new Element('div',{
+		'class': 'post',
+		'html': 'added'
+	}).inject(document.body);
+}
+
+new JustKidding({
+		ar: $(document.body).getElements('.post'),
+		populate: viewmore
+		});
+
+</script>
---
+++ class.justkidding.js
@@ -0,0 +1,73 @@
+var JustKidding = new Class({
+	
+	Implements: Options,
+	
+	options: {
+		it: 0,
+		ar: null,
+		populate: null,
+		keyEvent: 'keydown',
+		loop: false
+	},
+	
+	initialize: function(options){
+		if(options.populate != null && options.loop == true){
+			alert('either set a populate function or set looping, one or the other...');
+			return false;
+		}
+		this.setOptions(options); 
+		this.attachEvents();
+	},
+	
+	attachEvents: function(){
+		var that = this;
+		new Keyboard({
+		    defaultEventType: that.options.keyEvent, 
+		    active: true,
+		   	events: { 
+		        j: that.toNext.bind(that), //I would love to know how to make this custom
+		        k: that.toPrev.bind(that) //I tried dot notation for dynamic variables as well as... eval() =(
+		    }
+		});
+		
+	},
+	
+	toNext: function(){
+		
+		if(this.options.ar[this.options.it] == null && this.options.loop == false){
+			
+			/*Case for adding elements*/
+			if(this.options.populate != null){
+				this.options.populate(); //user function to add more DOM elements
+				this.reparse(); //reparse DOM tree
+			} 
+			else {
+				return false;
+			}
+			
+		}
+		
+		/*Case for looping*/
+		if(this.options.ar[this.options.it] == null && this.options.loop == true) {
+			new Fx.Scroll(window).toElement(this.options.ar[0]);
+			this.options.it = 0;
+		}
+		
+		new Fx.Scroll(window).toElement(this.options.ar[this.options.it]);
+		this.options.it = this.options.it + 1;
+		
+	},
+	
+	toPrev: function(){
+		
+		if(this.options.it != 1){ //?
+			new Fx.Scroll(window).toElement(this.options.ar[this.options.it-2]);
+			this.options.it = this.options.it - 1;
+		}
+	},
+	
+	reparse: function(){
+		this.options.ar = $(document.body).getElements('.post');
+	}
+	
+});
